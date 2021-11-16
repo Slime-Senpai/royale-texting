26a13f308b03fa06161e08fa9a517117dbb08ed9
Slime-Senpai <44924877+Slime-Senpai@users.noreply.github.com>
2021-11-16 21:33:05 +0100

    Start of project

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+cd /home/slimesenpaifr/royaletexting
+# a compléter avec les étapes nécessaire au lancement du projet
+#npm install
+#cd bin
+#pm2 restart www
+echo "ok ! Le site pour royale texting est on !"
